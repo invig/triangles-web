f7acb6ac01c361356f92c462d68492356477744d
Matt Salmon <matt.salmon@navitas.com>
2016-03-02 11:38:06 +1000

    Compress images at the proxy

--- app/webroot/ssl_proxy.php
+++ app/webroot/ssl_proxy.php
@@ -14,11 +14,16 @@
     }
 	
     if($strFile != ''){
+		
+		
         $cache_expire = 60*60*24*365;
         header("Pragma: public");
         header("Cache-Control: maxage=". $cache_expire);
         header('Expires: ' . gmdate('D, d M Y H:i:s', time() + $cache_expire).' GMT');
-        echo file_get_contents($strFile);
+
+		$im = new Imagick($strFile); 
+		$im->thumbnailImage(250,250); 
+        echo $im;
     }
 
     exit;
